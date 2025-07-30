aef8aa9c6afd2de0f6028cdf1d1a551186dac1ae
Sakshi9335 <sakshishu09@gmail.com>
2025-07-30 21:36:03 +0530

    initial commit

---
+++ hotelmenu.py
@@ -0,0 +1,31 @@
+# define the menu of restro
+menu ={
+    'pizza':40,
+    'pasta':50,
+    'Coffe':80,
+    'Burger':60,
+    'Salad':70,
+}
+#GREET
+print("Welcome to Pythn Restaurant")
+print("Pizza: Rs40\nPasta: Rs50\nCoffe: Rs80\nBurger: Rs60\nSalad: Rs70")
+
+order_total =0
+
+item_1 = input("Enter the name of item you want to order=")
+if item_1 in menu:
+    order_total += menu[item_1]
+    print(f"Your item {item_1} has been added to your order")
+    
+else:
+    print(f"Ordered item {item_1} is not available yet! ")  
+
+another_order = input("Do you want to add another item ?(Yes/No) ")
+if another_order == "Yes":
+    item_2 =input("enter the name of second item : ")
+    if item_2 in menu:
+        order_total += menu[item_2]
+        print(f"item {item_2}has been added to order")
+    else:
+        print(f"Ordered item {item_2} is not available!")
+print(f"The total amount of items to pay is {order_total}")  
---
+++ main.py
@@ -0,0 +1,2 @@
+print ("hote menu for restronet ")
+print (" my first mini project")
